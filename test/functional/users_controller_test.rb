43e586af2d12e92a1c564527913bd61af082474c
Miraks <none>
2011-12-16 15:12:13 +0400

    Add functional tests for users

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -1,49 +1,66 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  setup do
-    @user = users(:one)
-  end
-
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:users)
-  end
-
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
-  test "should create user" do
-    assert_difference('User.count') do
-      post :create, user: @user.attributes
+  context 'The user controller' do
+    setup do
+      @user = Factory.create(:user)
+      logout
     end
 
-    assert_redirected_to user_path(assigns(:user))
-  end
-
-  test "should show user" do
-    get :show, id: @user.to_param
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, id: @user.to_param
-    assert_response :success
-  end
-
-  test "should update user" do
-    put :update, id: @user.to_param, user: @user.attributes
-    assert_redirected_to user_path(assigns(:user))
-  end
-
-  test "should destroy user" do
-    assert_difference('User.count', -1) do
-      delete :destroy, id: @user.to_param
+    teardown do
+      logout
     end
 
-    assert_redirected_to users_path
+    context 'show action' do
+      should 'render' do
+        get :show, id: @user.to_param
+        assert_response :success
+      end
+    end
+
+    context 'new action' do
+      should 'render' do
+        get :new
+        assert_response :success
+      end
+    end
+
+    context 'edit action' do
+      should 'render' do
+        login @user
+        get :edit, id: @user.to_param
+        assert_responce :success
+      end
+    end
+
+    context 'update action' do
+      should 'be updated' do
+        old_pass = @user.crypted_password
+        login @user
+        put :update, id: @user.to_param, user: { password: 'new_pass', password_confirmation: 'new pass' }
+        assert_redirect_to user_path(@user)
+        @user.reload
+        assert_not_equal(old_pass, @user.crypted_password)
+      end
+    end
+
+    context 'destroy action' do
+      should 'be destroyed' do
+        login @user
+        delete :destroy, id: @user.to_param
+        assert_raise(ActiveRecord::RecordNotFound) do
+          @user.reload
+        end
+      end
+    end
+
+    context 'owner restricts' do
+      should 'be redirected' do
+        {edit: :get, update: :put, destroy: :delete}.each do |method, action|
+          send(action, method, id: @user.to_param)
+          assert_redirected_to root_path
+        end
+      end
+    end
   end
 end
