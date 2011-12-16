90ecbddb8a4d25d877427b7566af60862d3e46bf
Miraks <none>
2011-12-16 15:09:00 +0400

    Include user_session in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Microtube::Application.routes.draw do
   resources :users, except: [:index]
+
+  resources :user_sessions, only: [:new, :create, :destroy]
+  get 'login', to: "user_sessions#new"
+  delete 'logout', to: "user_sessions#destroy"
 end
