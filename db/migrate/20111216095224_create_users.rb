2d5dda8cadabe8ee1222ef3244a47661745841e7
Miraks <none>
2011-12-16 14:31:04 +0400

    Update restricts in create_users migration

--- db/migrate/20111216095224_create_users.rb
+++ db/migrate/20111216095224_create_users.rb
@@ -1,10 +1,10 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :login
-      t.string :crypted_password
-      t.string :password_salt
-      t.string :persistence_token
+      t.string :login, null: false
+      t.string :crypted_password, null: false
+      t.string :password_salt, null: false
+      t.string :persistence_token, null: false
 
       t.timestamps
     end
