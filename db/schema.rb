38ee1d57df937d635c04cc90d103ce9157830063
Miraks <none>
2011-12-16 19:36:54 +0400

    Add migration for association between user and videos

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111216120749) do
+ActiveRecord::Schema.define(:version => 20111216153158) do
 
   create_table "users", :force => true do |t|
     t.string   "login",             :null => false
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(:version => 20111216120749) do
     t.integer  "video_file_size",    :null => false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
 end
