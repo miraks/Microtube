5d210933dcf3c9d9287ea051dfe04def7d7185c9
Miraks <none>
2011-12-16 21:10:45 +0400

    Add validations and deafult name set method for post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,15 @@
 class Post < ActiveRecord::Base
   belongs_to :video
+
+  validates :message, :video, presence: true
+  validates :name, length: { maximum: 20 }
+  validates :message, length: { maximum: 500 }
+
+  before_save :set_name
+
+  private
+
+  def set_name
+    self.name = 'Anonymous' if self.name.empty?
+  end
 end
