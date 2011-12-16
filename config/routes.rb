1e3bcb51d37f1b50141e032c55c368be57a5921e
Miraks <none>
2011-12-16 19:36:02 +0400

    Set videos#index as root

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Microtube::Application.routes.draw do
-  resources :videos
-
   resources :users, except: [:index]
 
   resources :user_sessions, only: [:new, :create, :destroy]
   get 'login', to: "user_sessions#new"
   delete 'logout', to: "user_sessions#destroy"
+
+  resources :videos
+  root to: "videos#index"
 end
