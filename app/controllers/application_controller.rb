adabee308c88c31c46404059aa13d74a62d2076e
Miraks <none>
2011-12-16 15:36:19 +0400

    Add some methods to work with user session in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  helper_method :logged_in?, :current_user
+
+  def current_session
+    UserSession.find
+  end
+
+  def current_user
+    current_session.user
+  end
+
+  def logged_in?
+    current_session != nil
+  end
 end
