6658adae434d6f9a6c92d5d02091cfc11fe3774a
Miraks <none>
2011-12-16 13:41:41 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Microtube::Application.config.secret_token = 'a3cabbe3981e7c4303bff5b719de0b9c12710f3648bfa575865038974d685c222e75eb0860b1afbc3b13f005144ba01199a98422a801d783f24ce3bab73087f4'
