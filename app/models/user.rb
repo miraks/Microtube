725aca90a94ab7bb03cfe3fb7b19ac6dc4ccf4fa
Miraks <none>
2011-12-16 14:33:28 +0400

    Add validation in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+  attr_readonly :login
+
+  validates :login, length: 4..16
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,10 +14,10 @@
 ActiveRecord::Schema.define(:version => 20111216095224) do
 
   create_table "users", :force => true do |t|
-    t.string   "login"
-    t.string   "crypted_password"
-    t.string   "password_salt"
-    t.string   "persistence_token"
+    t.string   "login",             :null => false
+    t.string   "crypted_password",  :null => false
+    t.string   "password_salt",     :null => false
+    t.string   "persistence_token", :null => false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
